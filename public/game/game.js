c291de0abb4e2e8734c96e5621af6d1884057f9f
NDagger <yartsiz@gmail.com>
2025-11-10 18:19:16 +0100

    Player slide improved

--- public/game/game.js
+++ public/game/game.js
@@ -71,53 +71,6 @@ function movePointsFromCenter(points, distance) {
   return moved;
 }
 
-function pushPointOutsidePolygon(pos4, point) {
-  // Проверка, внутри ли точка многоугольника (алгоритм "ray casting")
-  function isPointInPolygon(polygon, p) {
-    let inside = false;
-    for (let i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
-      const xi = polygon[i].x, yi = polygon[i].y;
-      const xj = polygon[j].x, yj = polygon[j].y;
-      const intersect = ((yi > p.y) !== (yj > p.y)) &&
-        (p.x < (xj - xi) * (p.y - yi) / (yj - yi) + xi);
-      if (intersect) inside = !inside;
-    }
-    return inside;
-  }
-
-  // Нахождение ближайшей точки на рёбрах
-  function closestPointOnSegment(a, b, p) {
-    const abx = b.x - a.x, aby = b.y - a.y;
-    const apx = p.x - a.x, apy = p.y - a.y;
-    const t = Math.max(0, Math.min(1, (apx * abx + apy * aby) / (abx * abx + aby * aby)));
-    return { x: a.x + abx * t, y: a.y + aby * t };
-  }
-
-  // Основная логика
-  if (!isPointInPolygon(pos4, point)) return point;
-
-  let minDist = Infinity;
-  let closest = point;
-
-  for (let i = 0; i < pos4.length; i++) {
-    const a = pos4[i];
-    const b = pos4[(i + 1) % pos4.length];
-    const c = closestPointOnSegment(a, b, point);
-    const dist = Math.hypot(c.x - point.x, c.y - point.y);
-    if (dist < minDist) {
-      minDist = dist;
-      closest = c;
-    }
-  }
-
-  // Двигаем чуть-чуть наружу
-  const cx = point.x - closest.x;
-  const cy = point.y - closest.y;
-  const len = Math.hypot(cx, cy) || 1;
-  return { x: closest.x - cx / len * 0.001, y: closest.y - cy / len * 0.001 };
-}
-
-
 const degToRad = deg => deg * Math.PI / 180;
 
 const gameArrowLeft = document.getElementById('game-arrow-left');
@@ -340,9 +293,9 @@ export default class Game extends GameObject {
         }
 
         const steps = Math.max(Math.floor((240/(getFPS()||60))*this.#wallSpeedMult/10), 60)
-        const prevRotationOffset = this.#polygon.player.getRotationOffset();
         for (let i = 0; i < steps; i++) {
             if (this.#died) break
+            const prevRotationOffset = this.#polygon.player.getRotationOffset();
 
             if (this.#distanceDelay > 0) {
                 this.#distanceDelay -= frameTime * this.#wallSpeedMult / 5 / steps;
@@ -355,15 +308,20 @@ export default class Game extends GameObject {
                 this.#polygon.player.updatePosition()
             }
 
-            if (this.#getCollidingWall() !== undefined) {
+            let collidingWall = this.#getCollidingWall();
+            if (collidingWall !== undefined) {
                 // const side = closestSide(this.#polygon.player.getPointAbsolutePosition(), pos4)
                 this.#polygon.player.setRotationOffset(prevRotationOffset)
                 this.#polygon.player.updatePosition()
+
+                const point = pushPointOutsidePolygon(collidingWall.getVertexAbsolutePos4(), this.#polygon.player.getPointAbsolutePosition());
+
             };
+
 
             this.#walls = this.#updateWalls(this.#walls, frameTime, steps);
 
-            const collidingWall = this.#getCollidingWall();
+            collidingWall = this.#getCollidingWall();
             if (collidingWall !== undefined && !this.#died) {
                 const side = closestSide(this.#polygon.player.getPointAbsolutePosition(), collidingWall.getVertexAbsolutePos4())
                 if (side === 3) this.kill()
