25d923969766320b45e8c52df253055d9122aa0c
NDagger <yartsiz@gmail.com>
2025-12-06 13:07:02 +0100

    Levels are loaded when music loads

--- assets/SharedSystems-C4viSiVU.js
+++ assets/SharedSystems-C4viSiVU.js
@@ -1,4 +1,4 @@
-import{t as De,M as v,c as u,f as E,x as S,y as Ee,B as O,z as b,F as ae,S as Oe,T as m,o as B,e as F,H as Fe,p as U,G as Le,u as M,q as T,R as w,I as He,E as ze,J as ne,K as ie,L as oe,N as le,C as P,O as We,Q as A,W as Y,D as L,X as Ve,P as Ne,b as J,Y as X,a as je,v as $e,Z as qe}from"./index-BRV5wL-4.js";import{F as Ke,U as ue,G as Ye,h as Je,D as de,R as H,S as f,i as Q,j as ce,k as Xe,B as Qe}from"./colorToUniform-C-Yxljjp.js";import{S as Ze,B as Z,a as k}from"./Geometry-NfbVbt6-.js";var et=`in vec2 vMaskCoord;
+import{t as De,M as v,c as u,f as E,x as S,y as Ee,B as O,z as b,F as ae,S as Oe,T as m,o as B,e as F,H as Fe,p as U,G as Le,u as M,q as T,R as w,I as He,E as ze,J as ne,K as ie,L as oe,N as le,C as P,O as We,Q as A,W as Y,D as L,X as Ve,P as Ne,b as J,Y as X,a as je,v as $e,Z as qe}from"./index-BHhORINT.js";import{F as Ke,U as ue,G as Ye,h as Je,D as de,R as H,S as f,i as Q,j as ce,k as Xe,B as Qe}from"./colorToUniform-DeRIbLZl.js";import{S as Ze,B as Z,a as k}from"./Geometry-B82wW9lM.js";var et=`in vec2 vMaskCoord;
 in vec2 vTextureCoord;
 
 uniform sampler2D uTexture;
